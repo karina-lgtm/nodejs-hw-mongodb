ec728c4a9aa66423dd537021a30c5781672aab86
Karina Myronova <km0023549@gmail.com>
2025-07-28 10:59:03 +0200

    hw3

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,14 +1,22 @@
 import mongoose from 'mongoose';
-import process from 'process';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const {
+  MONGODB_USER,
+  MONGODB_PASSWORD,
+  MONGODB_URL,
+  MONGODB_DB
+} = process.env;
+
+const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
 export const initMongoConnection = async () => {
   try {
-    const { MONGODB_URI } = process.env;
-
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(uri);
     console.log('Mongo connection successfully established!');
-  } catch (error) {
-    console.error('Mongo connection error:', error);
-    throw error;
+  } catch (err) {
+    console.error('Mongo connection error:', err.message);
+    process.exit(1);
   }
 };
