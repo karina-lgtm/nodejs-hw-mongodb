6263b5d13f2b2ec9a239dcd78d28beac8b05ba9b
Karina Myronova <km0023549@gmail.com>
2025-07-29 13:48:38 +0200

    Fix: return contacts from MongoDB in hw2

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,28 +1,10 @@
-import { getAllContacts } from '../services/contacts.js';
-import { getContactById } from '../services/contacts.js';
+import { Contact } from '../models/contact.js';
 
 export const getContacts = async (req, res) => {
-  const contacts = await getAllContacts();
+  const contacts = await Contact.find(); // ← беремо з Mongo
   res.status(200).json({
     status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
-  });
-};
-
-export const getContact = async (req, res) => {
-  const { contactId } = req.params;
-  const contact = await getContactById(contactId);
-
-  if (!contact) {
-    return res.status(404).json({
-      message: 'Contact not found',
-    });
-  }
-
-  res.status(200).json({
-    status: 200,
-    message: `Successfully found contact with id ${contactId}!`,
-    data: contact,
   });
 };
