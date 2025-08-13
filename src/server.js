93875b82910b873dc5922ff83dae71a53cd471c9
Karina Myronova <km0023549@gmail.com>
2025-08-13 17:37:45 +0300

    hw5 serverjs

--- src/server.js
+++ src/server.js
@@ -2,17 +2,12 @@ import express from 'express';
 import pinoHttp from 'pino-http';
 import pino from 'pino';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 
 import { getEnvVariable } from './utils/getEnvVar.js';
-
-import authRouter from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
-
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { authenticate } from './middlewares/authenticate.js';
 
 const PORT = getEnvVariable('PORT') || 5150;
 
@@ -21,17 +16,23 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
-  app.use(cookieParser());
 
-  const logger = pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  });
+  const isDev = process.env.NODE_ENV === 'development';
+
+  const logger = isDev
+    ? pino({
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+          },
+        },
+      })
+    : pino(); // для Render
+
   app.use(pinoHttp({ logger }));
 
-  app.use('/auth', authRouter);
-  app.use('/contacts', authenticate, contactsRouter);
+  app.use(contactsRouter);
 
   app.use(notFoundHandler);
 
@@ -41,6 +42,6 @@ export const setupServer = () => {
     if (error) {
       throw error;
     }
-    logger.info(`Server is runing on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
   });
 };
